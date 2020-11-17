5ea82494a5bc700e4be942d30e3e8f86f63c1deb
Tim Bogers <t.bogers@hotmail.com>
2020-11-17 14:57:59 +0100

    Create MiniMaxSum.cs

---
+++ MiniMaxSum.cs
@@ -0,0 +1,33 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr) {
+        Array.Sort(arr);
+        long min = 0, max = 0;
+        var longs = arr.Select(item => (long)item).ToArray();
+        max = longs.Sum() - arr[0];
+        min = longs.Sum() - arr[4];
+        Console.WriteLine(min + " " + max); 
+
+    }
+
+    static void Main(string[] args) {
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        miniMaxSum(arr);
+    }
+}
